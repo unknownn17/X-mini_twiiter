a679611fef5b7ad1e9cbef8143c540c9ccdc6228
Your Name <dostonxoshimov2005@gmail.com>
2025-01-28 15:13:04 +0500

    server ip-added

--- api_gateway/internal/api/handler/handler.go
+++ api_gateway/internal/api/handler/handler.go
@@ -21,7 +21,7 @@ type Handler struct {
 // @in header
 // @name Authorization
 // @description Enter the token in the format `Bearer {token}`
-// @host localhost:7777
+// @host 54.93.169.32:7777
 // @BasePath /
 
 
